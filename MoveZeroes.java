b4d4ccf938bec7f283f7810421905b710a5e7ba7
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-12 08:54:16 -0700

    Create MoveZeroes.java

---
+++ MoveZeroes.java
@@ -0,0 +1,32 @@
+class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+
+        int index = 0;
+
+        for (int num : nums) {
+            if (num != 0) {
+                nums[index] = num;
+                index++;
+            }
+        }
+
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 1, 0, 3, 12};
+
+        moveZeroes(nums);
+
+        System.out.print("Result: ");
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
